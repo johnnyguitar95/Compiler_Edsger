f813e8aa0fa8cbd058012344e6e3705bdb1c7759
John Patrick Halloran <jhallor@mscs.mu.edu>
2017-03-08 23:50:04 -0600

    added my name

--- Assignment4/VisitorPkg/TreePrintVisitor.java
+++ Assignment4/VisitorPkg/TreePrintVisitor.java
@@ -5,13 +5,7 @@ import ParsePkg.*;
 
 public class TreePrintVisitor implements Visitor {
 
-  // Print out AbstractSyntax trees (mostly for
-  // debugging the parser).
-  // I didn't take the time to print the expression
-  // trees, but you certainly could do that.
-  //
-  // mike slattery - mar 2005
-
+  // Author John Halloran
   String indent = "";
 
   void doIndent() {
@@ -296,90 +290,3 @@ public class TreePrintVisitor implements Visitor {
   }
  
 }
-
-/*
-  public void visit(Assign n){
-    System.out.print(indent);
-    System.out.print("Assign(");
-    System.out.print(n.idname);
-    System.out.println(", ");
-    doIndent();
-    System.out.print(indent);
-    n.e.accept(this);
-    undoIndent();
-    System.out.println(")");
-  }
-
-  public void visit(Output n){
-    System.out.print(indent);
-    System.out.print("Output(");
-    doIndent();
-    for (int i = 0; i < n.elist.size(); i++){
-      if (i > 0){
-	System.out.println(", ");
-        System.out.print(indent);
-      }
-      n.elist.get(i).accept(this);
-    }
-    undoIndent();
-    System.out.println(")");
-  }
-
-  public void visit(Unless n){
-    System.out.print(indent);
-    System.out.println("Unless(");
-    doIndent();
-    for (int i = 0; i < n.slist.size(); i++){
-      n.slist.get(i).accept(this);
-      System.out.print(indent);
-      System.out.println(",");
-    }
-    System.out.print(indent);
-    n.cond.accept(this);
-    undoIndent();
-    System.out.println(")");
-  }
-
-  public void visit(AbstractSyntaxPkg.Number n){
-    System.out.print(n.value);
-  }
-
-  public void visit(Id n){
-    System.out.print(n.name);
-  }
-
-  public void visit(Plus n){
-    System.out.print("(");
-    n.e1.accept(this);
-    System.out.print(" + ");
-    n.e2.accept(this);
-    System.out.print(")");
-  }
-
-  public void visit(Minus n){
-    System.out.print("(");
-    n.e1.accept(this);
-    System.out.print(" - ");
-    n.e2.accept(this);
-    System.out.print(")");
-  }
-
-  public void visit(Times n){
-    n.e1.accept(this);
-    System.out.print(" * ");
-    n.e2.accept(this);
-  }
-
-  public void visit(Divide n){
-    n.e1.accept(this);
-    System.out.print(" / ");
-    n.e2.accept(this);
-  }
-
-  public void visit(Leq n){
-    n.e1.accept(this);
-    System.out.print(" <= ");
-    n.e2.accept(this);
-  }
-
-}*/
